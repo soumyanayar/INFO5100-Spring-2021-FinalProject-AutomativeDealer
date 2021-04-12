17926e0fa7a883df7f60f1680b44d6a05b2bbcb1
RY-Xia <xia.ru@northeastern.edu>
2021-04-12 22:34:06 +0800

    Add files via upload

--- group6/src/group6/main/FormAction.java
+++ group6/src/group6/main/FormAction.java
@@ -1,37 +1,36 @@
 package group6.main;
 
-import group8.Main.Dealer;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * @author Runyao Xia
  * @date: 2021/4/10
  */
 public class FormAction {
-    private List<User> users;
-    private Dealer dealer;
+    private User user;
+    private Integer id;
 
-    public FormAction(Dealer dealer) {
-        users = new ArrayList<>();
-        this.dealer = dealer;
+    public FormAction(User user) {
+        this.user = user;
+        user.setForm(this);
     }
-    public User newUser(String firstName, String lastName, String eMail, String phoneNumber, String zipCode) {
-        User user = new User(firstName, lastName, eMail, phoneNumber, zipCode);
-        user.assignForm(this);
+
+    public User getUser() {
         return user;
     }
 
-    public List<User> getUsers() {
-        return users;
+    @Override
+    public String toString() {
+        return "FormAction{" +
+                "user=" + user +
+                '}';
     }
 
-    public Dealer getDealer() {
-        return dealer;
+    public Integer getId() {
+        return id;
     }
 
-    public void setDealer(Dealer dealer) {
-        this.dealer = dealer;
+    public void setId(Integer id) {
+        this.id = id;
     }
 }
+
