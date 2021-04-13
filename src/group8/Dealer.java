ebb21948db5f986c65b13a72831a5272da13496c
Lakshmi SS <sslakshmi@outlook.com>
2021-04-12 21:21:34 -0700

    Add Dealer class properties and methods in DealerDirectory to query data from SQL Server

--- src/group8/Dealer.java
+++ src/group8/Dealer.java
@@ -1,29 +1,106 @@
 package group8;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author jasmineshin
- */
 public class Dealer {
     private String dealerID;
     private String name;
-    private long num; //phonenumber
+    private String streetAddress;
+    private String city;
+    private String stateID;
+    private String state;
+    private String zipcode;
+    private String phoneNumber;
     private Inventory inventory;
-    
-    public Dealer(String dealerID, String name, long num){
+
+    public Dealer() {}
+
+    public Dealer(String dealerID, String name, String streetAddress, String city, String stateID, String state,
+                  String zipcode, String phoneNumber, Inventory inventory) {
         this.dealerID = dealerID;
         this.name = name;
-        this.num = num;
-        inventory = new Inventory(this);
+        this.streetAddress = streetAddress;
+        this.city = city;
+        this.stateID = stateID;
+        this.state = state;
+        this.zipcode = zipcode;
+        this.phoneNumber = phoneNumber;
+        this.inventory = inventory;
     }
-    public String getName(){
+
+    /*******************  GETTER  *******************/
+    public String getDealerID() {
+        return dealerID;
+    }
+
+    public String getStreetAddress() {
+        return streetAddress;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getStateID() {
+        return stateID;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public String getZipcode() {
+        return zipcode;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public Inventory getInventory() {
+        return inventory;
+    }
+
+    public String getName() {
         return name;
     }
+
+    /*******************  SETTER  *******************/
+
+    public void setDealerID(String dealerID) {
+        this.dealerID = dealerID;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setStreetAddress(String streetAddress) {
+        this.streetAddress = streetAddress;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public void setStateID(String stateID) {
+        this.stateID = stateID;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public void setZipcode(String zipcode) {
+        this.zipcode = zipcode;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void setInventory(Inventory inventory) {
+        this.inventory = inventory;
+    }
+
+    /*******************  METHODS  *******************/
 
 }
--- src/group8/DealerDirectory.java
+++ src/group8/DealerDirectory.java
@@ -1,22 +1,42 @@
 package group8;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author jasmineshin
- */
+import java.util.List;
 
 public class DealerDirectory {
-    private ArrayList<Dealer> dealerDirectory;
-    
-    public DealerDirectory(){
-        dealerDirectory = new ArrayList<>();
+
+    private utils.JDBC jdbcInstance;
+
+    public DealerDirectory() {
+        try {
+            jdbcInstance = utils.JDBC.getInstance();
+        } catch (SQLException exception) {
+            exception.printStackTrace();
+        }
+    }
+
+
+    /*******************  METHODS  *******************/
+
+    public List<Dealer> getDealerByStateOrStateId(String stateOrStateId) throws SQLException {
+        List<Dealer> dealers = new ArrayList<>();
+        String query = "select * from dealer where state = ? or state_id = ?";
+        String[] params = new String[]{stateOrStateId, stateOrStateId};
+        ResultSet resultSet = jdbcInstance.query(query, params);
+        while(resultSet.next()){
+            Dealer dealer = new Dealer();
+            dealer.setDealerID(resultSet.getString("id"));
+            dealer.setName(resultSet.getString("name"));
+            dealer.setStreetAddress(resultSet.getString("street_address"));
+            dealer.setCity(resultSet.getString("city"));
+            dealer.setStateID(resultSet.getString("state_id"));
+            dealer.setState(resultSet.getString("state"));
+            dealer.setZipcode(resultSet.getString("zipcode"));
+            dealer.setPhoneNumber(resultSet.getString("phone_number"));
+            dealers.add(dealer);
+        }
+        return dealers;
     }
 }
