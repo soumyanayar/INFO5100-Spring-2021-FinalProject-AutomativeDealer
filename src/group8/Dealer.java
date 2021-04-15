70c2b795d77e2c1be80acbde8e8fa54d92a50af8
Lakshmi SS <sslakshmi@outlook.com>
2021-04-15 11:35:11 -0700

    Read data from SQL server

--- src/group8/Dealer.java
+++ src/group8/Dealer.java
@@ -10,8 +10,11 @@ public class Dealer {
     private String zipcode;
     private String phoneNumber;
     private String country;
+    private double latitude;
+    private double longitude;
 
-    public Dealer() {}
+    public Dealer() {
+    }
 
     public Dealer(String dealerID, String name, String streetAddress, String city, String stateID, String state,
                   String zipcode, String phoneNumber, String country, Inventory inventory) {
@@ -26,79 +29,93 @@ public class Dealer {
         this.country = country;
     }
 
-    /*******************  GETTER  *******************/
+    /*******************  GETTERS AND SETTERS  *******************/
     public String getDealerID() {
         return dealerID;
+    }
+
+    public void setDealerID(String dealerID) {
+        this.dealerID = dealerID;
     }
 
     public String getStreetAddress() {
         return streetAddress;
     }
 
-    public String getCity() {
-        return city;
-    }
-
-    public String getStateID() {
-        return stateID;
-    }
-
-    public String getState() {
-        return state;
-    }
-
-    public String getZipcode() {
-        return zipcode;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getCountry() {
-        return country;
-    }
-
-    /*******************  SETTER  *******************/
-
-    public void setDealerID(String dealerID) {
-        this.dealerID = dealerID;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public void setStreetAddress(String streetAddress) {
         this.streetAddress = streetAddress;
+    }
+
+    public String getCity() {
+        return city;
     }
 
     public void setCity(String city) {
         this.city = city;
     }
 
+    public String getStateID() {
+        return stateID;
+    }
+
     public void setStateID(String stateID) {
         this.stateID = stateID;
+    }
+
+    public String getState() {
+        return state;
     }
 
     public void setState(String state) {
         this.state = state;
     }
 
+    public String getZipcode() {
+        return zipcode;
+    }
+
     public void setZipcode(String zipcode) {
         this.zipcode = zipcode;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
     }
 
     public void setPhoneNumber(String phoneNumber) {
         this.phoneNumber = phoneNumber;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
     public void setCountry(String country) {
         this.country = country;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
     }
 
     /*******************  METHODS  *******************/
--- src/group8/DealerDirectory.java
+++ src/group8/DealerDirectory.java
@@ -1,5 +1,7 @@
 package group8;
 
+import group8.data.NewJDBC;
+
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
@@ -7,14 +9,21 @@ import java.util.List;
 
 public class DealerDirectory {
 
-    private utils.JDBC jdbcInstance;
+    private NewJDBC jdbcInstance;
 
     public DealerDirectory() {
         try {
-            jdbcInstance = utils.JDBC.getInstance();
-        } catch (SQLException exception) {
+            jdbcInstance = NewJDBC.getInstance();
+        } catch (SQLException | ClassNotFoundException exception) {
             exception.printStackTrace();
         }
+    }
+
+    public static void main(String[] args) throws SQLException {
+        DealerDirectory dealerDirectory = new DealerDirectory();
+        List<Dealer> dealers = dealerDirectory.getDealerByStateOrStateId("WA");
+        System.out.println(dealers.size());
+        System.out.println(dealerDirectory.getDealerByZipCode("98032"));
     }
 
 
@@ -22,21 +31,38 @@ public class DealerDirectory {
 
     public List<Dealer> getDealerByStateOrStateId(String stateOrStateId) throws SQLException {
         List<Dealer> dealers = new ArrayList<>();
-        String query = "select * from dealer where state = ? or state_id = ?";
+        String query = "select * from DealerWithState where State = ? or StateCode = ?";
         String[] params = new String[]{stateOrStateId, stateOrStateId};
         ResultSet resultSet = jdbcInstance.query(query, params);
-        while(resultSet.next()){
-            Dealer dealer = new Dealer();
-            dealer.setDealerID(resultSet.getString("id"));
-            dealer.setName(resultSet.getString("name"));
-            dealer.setStreetAddress(resultSet.getString("street_address"));
-            dealer.setCity(resultSet.getString("city"));
-            dealer.setStateID(resultSet.getString("state_id"));
-            dealer.setState(resultSet.getString("state"));
-            dealer.setZipcode(resultSet.getString("zipcode"));
-            dealer.setPhoneNumber(resultSet.getString("phone_number"));
-            dealers.add(dealer);
+        while (resultSet.next()) {
+            dealers.add(getDealer(resultSet));
         }
         return dealers;
+    }
+
+    public List<Dealer> getDealerByZipCode(String zipcode) throws SQLException {
+        List<Dealer> dealers = new ArrayList<>();
+        String query = "select * from DealerWithState where ZipCode = ?";
+        String[] params = new String[]{zipcode};
+        ResultSet resultSet = jdbcInstance.query(query, params);
+        while (resultSet.next()) {
+            dealers.add(getDealer(resultSet));
+        }
+        return dealers;
+    }
+
+    private Dealer getDealer(ResultSet resultSet) throws SQLException {
+        Dealer dealer = new Dealer();
+        dealer.setDealerID(resultSet.getString("DealerId"));
+        dealer.setName(resultSet.getString("DealerName"));
+        dealer.setStreetAddress(resultSet.getString("DealerAddress"));
+        dealer.setCity(resultSet.getString("City"));
+        dealer.setStateID(resultSet.getString("StateCode"));
+        dealer.setState(resultSet.getString("State"));
+        dealer.setZipcode(resultSet.getString("ZipCode"));
+        dealer.setPhoneNumber(resultSet.getString("PhoneNumber"));
+        dealer.setLatitude(resultSet.getDouble("Latitude"));
+        dealer.setLongitude(resultSet.getDouble("Longitude"));
+        return dealer;
     }
 }
