1003ae525d5d17a2b0a857ac0ce1cac3550bf463
YangziXinJas <60373476+YangziXinJas@users.noreply.github.com>
2021-04-16 20:54:47 -0700

    added dao

---
+++ src/group8/data/MainTest.java
@@ -0,0 +1,28 @@
+package group8.data;
+
+import group8.Car;
+import group8.DealerDirectory;
+import java.sql.SQLException;
+import java.util.List;
+
+/**
+ * @author Guiyu Liu
+ * Apr 13, 2021.
+ */
+public class MainTest {
+
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+//        DbDealerStorage dbDealerStorage = new DbDealerStorage();
+//        //dbDealerStorage.printAll();
+//        //dbDealerStorage.printNumRow();
+//        //dbDealerStorage.printNumCol();
+//        //dbDealerStorage.printColInfo();
+//        List<Car> res = dbDealerStorage.getAllCarsByDealerId("18");
+//        for(Car c : res){
+//           System.out.println(c.getMake());
+//        }
+//        DealerDirectory d = dbDealerStorage.loadDealerDirectory();
+//        System.out.println(d.toString());
+//        //System.out.println(dbDealerStorage.getDealerById("testId").getName());
+    }
+}
