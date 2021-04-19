7f14ba785ad52889d96887d3f0f3f87963ee839d
Lakshmi SS <sslakshmi@outlook.com>
2021-04-19 01:36:14 -0700

    Remove redundant class

--- src/group8/Driver.java
+++ src/group8/Driver.java
@@ -1,6 +1,5 @@
 package group8;
 
-import group8.data.DataBaseLoader;
 import java.sql.SQLException;
 
 /*
@@ -17,12 +16,12 @@ public class Driver {
     
     public static void main(String[] args) throws SQLException, ClassNotFoundException{
         // load all data from database into database loader
-        DataBaseLoader database = new DataBaseLoader();
+//        DataBaseLoader database = new DataBaseLoader();
         
-        DealerDirectory dir = database.getDealerDirectory();
+//        DealerDirectory dir = database.getDealerDirectory();
         // display directory content
         // output contains info about dealer and cars in the dealer's inventory
-        System.out.println(dir.toString());
+//        System.out.println(dir.toString());
     }
     
 }
