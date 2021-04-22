f03bbbfb6817cee8acc251037db6805836af40ca
andyfan92 <txair921202@gmail.com>
2021-04-22 09:06:37 +0800

    fix

--- src/group2/dao/VehicleDAO.java
+++ src/group2/dao/VehicleDAO.java
@@ -30,12 +30,14 @@ public class VehicleDAO {
     }
 
     public List<Map<String, Object>> getById(int id) throws SQLException {
-        final ResultSet query = newJDBC.query("select * from vehicle_test vt inner join model_test mot on vt.model_id = mot.model_id\n" +
-                "inner join make_test mat on vt.make_id = mat.make_id inner join Dealer d on vt.dealer_id =\n" +
-                "d.DealerId\n" +
-                "where vehicle_id = ?", new String[]{String.valueOf(id)});
+//        final ResultSet query = newJDBC.query("select * from vehicle_test vt inner join model_test mot on vt.model_id = mot.model_id\n" +
+//                "inner join make_test mat on vt.make_id = mat.make_id inner join Dealer d on vt.dealer_id =\n" +
+//                "d.DealerId\n" +
+//                "where vehicle_id = ?", new String[]{String.valueOf(id)});
+        final ResultSet query = newJDBC.query("select * from NewVehicleData nwd inner " +
+                "join Dealer d on nwd.DealerId = d.DealerId  where VehicleId = ?", new String[]{String.valueOf(id)});
         List<Map<String, Object>> res = Utils.resultSetToList(query);
-        Car car = Utils.transToCar(res.get(0));
+//        Car car = Utils.transToCar(res.get(0));
         return res;
     }
 
