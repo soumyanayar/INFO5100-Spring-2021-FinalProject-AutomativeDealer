4b715c0668d75ea7859e02623a606db387daab6f
Palak-M <mattoo.p@northeastern.edu>
2021-04-22 09:27:49 -0700

    Added comments

--- src/group6/DealerFont.java
+++ src/group6/DealerFont.java
@@ -4,41 +4,41 @@ import java.awt.*;
 
 public class DealerFont {
 
-    static String FONT_NAME = "Helvetica";
-    static int TITLE_FONT_SIZE = 30;
-    static int HEADER_FONT_SIZE = 20;
-    static int TITLE_2_FONT_SIZE = 18;
-    static int TITLE_3_FONT_SIZE = 16;
-    static int NORMAL_FONT_SIZE = 14;
-    static int DESCRIPTION_FONT_SIZE = 12;
+    private static final String FONT_NAME = "Helvetica";
+    private static final int TITLE_FONT_SIZE = 30;
+    private static final int HEADER_FONT_SIZE = 20;
+    private static final int TITLE_2_FONT_SIZE = 18;
+    private static final int TITLE_3_FONT_SIZE = 16;
+    private static final int NORMAL_FONT_SIZE = 14;
+    private static final int DESCRIPTION_FONT_SIZE = 12;
 
-    static Font getHeaderFont() {
+    public static Font getHeaderFont() {
         Font font = new Font(FONT_NAME, Font.PLAIN, HEADER_FONT_SIZE);
         GraphicsEnvironment genv = GraphicsEnvironment.getLocalGraphicsEnvironment();
         genv.registerFont(font);
         return font;
     }
 
-    static Font getTitleFont() {
+    public static Font getTitleFont() {
         return new Font(FONT_NAME, Font.BOLD, TITLE_FONT_SIZE);
     }
 
-    static Font getSubTitleFont() {
+    public static Font getSubTitleFont() {
         return new Font(FONT_NAME, Font.PLAIN, TITLE_2_FONT_SIZE);
     }
 
-    static Font getSubSubTitleFont() {
+    public static Font getSubSubTitleFont() {
         return new Font(FONT_NAME, Font.PLAIN, TITLE_3_FONT_SIZE);
     }
 
-    static Font getDescriptionFont() {
+    public static Font getDescriptionFont() {
         Font font = new Font(FONT_NAME, Font.PLAIN, DESCRIPTION_FONT_SIZE);
         GraphicsEnvironment genv = GraphicsEnvironment.getLocalGraphicsEnvironment();
         genv.registerFont(font);
         return font;
     }
 
-    static Font getNormalFont() {
+    public static Font getNormalFont() {
         Font font = new Font(FONT_NAME, Font.PLAIN, NORMAL_FONT_SIZE);
         GraphicsEnvironment genv = GraphicsEnvironment.getLocalGraphicsEnvironment();
         genv.registerFont(font);
