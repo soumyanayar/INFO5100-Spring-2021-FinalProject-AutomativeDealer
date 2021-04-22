d4c660d0d3efc2727eab904fd6df07d41d18e7d2
zhehui07 <77646339+zhehui07@users.noreply.github.com>
2021-04-21 19:02:30 -0700

    Add files via upload
    split init function both in LeadFroms and LeadForm class, and deleted the main function in RespondDetailsUI class

--- src/group3/LeadForms.java
+++ src/group3/LeadForms.java
@@ -15,8 +15,10 @@ public class LeadForms extends JFrame implements ItemListener {
     private int height;
     private int width;
     private Map<String, List<LeadForm>> modelToLeadsMap;
+    private JPanel mainPanel;
     private JPanel listingPanel;
-
+    private JPanel optionPanel;
+    private JComboBox<String> filteredByComboBox;
     public LeadForms(String title) {
         super(title);
         width = 750;
@@ -25,13 +27,26 @@ public class LeadForms extends JFrame implements ItemListener {
     }
 
     public void init(List<Lead> leads) {
+        super.setSize(500, 300);
+        mainPanel = new JPanel();
+        super.setContentPane(mainPanel);
         this.numberOfLeads = leads.size();
         height = numberOfLeads * 400;
         listingPanel.setLayout(new GridLayout(numberOfLeads, 1));
-        JPanel optionPanel = new JPanel();
-        optionPanel.setSize(750, 10);
-        JPanel mainPanel = new JPanel();
         mainPanel.setLayout(new GridBagLayout());
+        createLeads(leads);
+        setFilter();
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.pack();
+        this.setVisible(true);
+    }
+
+    public void setFilter(){
+        optionPanel = new JPanel();
+        optionPanel.setSize(750, 10);
+        optionPanel.add(new JLabel("Filter By "));
+        optionPanel.add(filteredByComboBox);
+        optionPanel.setSize(new Dimension(750, 100));
         GridBagConstraints controlLayout = new GridBagConstraints();
         controlLayout.fill = GridBagConstraints.VERTICAL;
         controlLayout.gridy = 0;
@@ -41,12 +56,10 @@ public class LeadForms extends JFrame implements ItemListener {
         controlLayout.gridy = 1;
         controlLayout.gridwidth = 3;
         mainPanel.add(new JScrollPane(listingPanel), controlLayout);
-        super.setContentPane(mainPanel);
-        optionPanel.add(new JLabel("Filter By "));
-        JComboBox<String> filteredByComboBox = new JComboBox<String>();
-        optionPanel.add(filteredByComboBox);
-        optionPanel.setSize(new Dimension(750, 100));
-        // super.setContentPane(new JScrollPane(listingPanel));
+    }
+
+    public void createLeads(List<Lead> leads){
+        filteredByComboBox = new JComboBox<String>();
         for (int i = 0; i < numberOfLeads; i++) {
             Lead lead = leads.get(i);
             System.out.println("Current lead name is" + lead.getLastName());
@@ -64,58 +77,7 @@ public class LeadForms extends JFrame implements ItemListener {
             }
         }
         filteredByComboBox.addItemListener(this);
-//        super.add(BorderLayout.CENTER, new JScrollPane(mainPanel));
-        super.setSize(500, 300);
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.pack();
-        this.setVisible(true);
     }
-
-    public void initOrigin(List<Lead> leads) {
-        this.numberOfLeads = leads.size();
-        height = numberOfLeads * 400;
-        listingPanel.setLayout(new GridLayout(numberOfLeads, 1));
-        JPanel optionPanel = new JPanel();
-        JPanel mainPanel = new JPanel();
-        mainPanel.setLayout(new GridLayout(2, 1));
-        mainPanel.add(optionPanel);
-        mainPanel.add(new JScrollPane(listingPanel));
-        super.setContentPane(mainPanel);
-        optionPanel.add(new JLabel("Filter By "));
-        JComboBox<String> filteredByComboBox = new JComboBox<String>();
-        optionPanel.add(filteredByComboBox);
-        optionPanel.setSize(new Dimension(750, 100));
-        // super.setContentPane(new JScrollPane(listingPanel));
-        for (int i = 0; i < numberOfLeads; i++) {
-            Lead lead = leads.get(i);
-            System.out.println("Current lead name is" + lead.getLastName());
-            LeadForm leadForm = new LeadForm();
-            leadForm.init(this, lead);
-            leadForm.getMainPanel().setSize(750, 100);
-            listingPanel.add(leadForm.getMainPanel(), BorderLayout.NORTH);
-            if (modelToLeadsMap.get(lead.getCarModel().toUpperCase()) != null) {
-                modelToLeadsMap.get(lead.getCarModel().toUpperCase()).add(leadForm);
-            } else {
-                filteredByComboBox.addItem(lead.getCarModel().toUpperCase());
-                List<LeadForm> tmpLeadFormList = new ArrayList<>();
-                tmpLeadFormList.add(leadForm);
-                modelToLeadsMap.put(lead.getCarModel().toUpperCase(), tmpLeadFormList);
-            }
-        }
-        filteredByComboBox.addItemListener(this);
-        super.setSize(width, 300);
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.pack();
-        this.setVisible(true);
-    }
-
-    public static void main(String[] args) {
-        LeadForms frame = new LeadForms("Leads List");
-        LeadDataAccessor lda = new LeadDataAccessor("D:\\NewVehicleData.csv");
-        frame.init(lda.getLeads());
-
-    }
-
     public void updateFrame(JFrame frame){
         frame.setSize(750, numberOfLeads * 100);
     }
@@ -124,8 +86,6 @@ public class LeadForms extends JFrame implements ItemListener {
     public void setSize(int width, int height) {
         System.out.println("height is " + this.height);
         this.height += height;
-        // super.setSize(this.width, this.height);
-        // super.getContentPane().getComponents()[0].setSize(this.width, this.height);
     }
 
     @Override
